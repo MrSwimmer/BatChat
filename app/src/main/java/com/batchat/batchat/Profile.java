45d55e028cb12db12d40be2ac60b729a52a5b249
MrSwimmer <mrswimmerlab@gmail.com>
2017-07-05 22:02:44 +0700

    Success Enter and Registartion

---
+++ app/src/main/java/com/batchat/batchat/Profile.java
@@ -0,0 +1,20 @@
+package com.batchat.batchat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Севастьян on 05.07.2017.
+ */
+
+public class Profile extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.profile, container, false);
+        return v;
+    }
+}
